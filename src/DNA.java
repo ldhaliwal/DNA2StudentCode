b4ad46e82fb0a3a3f30a78072b20b8ac58611c24
ldhaliwal <liliana.dhaliwal@menloschool.org>
2024-10-21 21:55:06 -0700

    a little more work, variables are confusing

--- src/DNA.java
+++ src/DNA.java
@@ -13,40 +13,27 @@ import java.util.ArrayList;
  */
 
 public class DNA {
+    radix = 256;
+    prime = 2147483647L;
+
     public static int STRCount(String sequence, String STR) {
         char startSTR = STR.charAt(0);
         int strLength = STR.length();
-        ArrayList<Integer> strInstances = new ArrayList<>();
+        int longestRun = 0;
 
-//        // Map each letter to a binary representation
-//        int[] binaryMap = new int[256];
-//        binaryMap['A'] = 0b00;
-//        binaryMap['C'] = 0b01;
-//        binaryMap['G'] = 0b10;
-//        binaryMap['T'] = 0b11;
-//
-//        int strBinary = 0;
-//
-//        // Convert the STR to binary
-//        for (int i = 0; i < strLength; i++) {
-//            strBinary = (strBinary << 2) | binaryMap[STR.charAt(i)];
-//        }
-//
-//        int currentBinary = 0;
-//
-//        // Iterate through the sequence
-//        for (int i = 0; i < sequence.length(); i++) {
-//            // Shift the binary representation and add the new character
-//            currentBinary = ((currentBinary << 2) | binaryMap[sequence.charAt(i)]) & ((1 << (2 * strLength)) - 1);
-//
-//            // If enough characters have been processed, compare the two binary values
-//            if (i >= strLength - 1) {
-//                if (currentBinary == strBinary) {
-//                    strInstances.add(i - strLength + 1);
-//                    i += strLength - 1;
-//                }
-//            }
-//        }
+        int strHash = 0;
+        int windowHash = 0;
+
+        // Compute hash of STR and first window of sequence
+        for (int i = 0; i < strLength; i++) {
+            strHash = (int) ((radix * strHash + STR.charAt(i)) % prime);
+            windowHash = (int) ((radix * windowHash + sequence.charAt(i)) % prime);
+        }
+
+        //Recursive
+
+
+        ArrayList<Integer> strInstances = new ArrayList<>();
 
 
         // Iterate through the sequence
@@ -85,5 +72,34 @@ public class DNA {
 
         // Return the result; if no repeats were found, longestRepeated remains 0, so we return the actual count
         return longestRepeated;
+    }
+
+
+    public int recursive(String STR, String window, int count, int longestRun){
+        if(hash(STR) == hash(window)){
+            count++;
+            // shift window by 3
+            return recursive(STR, window, count, longestRun);
+        }
+        else{
+            // shift window by 1
+            if(count > longestRun){
+                longestRun = count;
+            }
+            count = 0;
+            recursive(STR, window, count, longestRun);
+        }
+
+        return 0;
+    }
+
+    public long hash(String text){
+        int length = text.length();
+        long hash = 0;
+
+        for(int i = 0; i < length; i++) {
+            hash = (hash * radix + text.charAt(i)) % prime;
+        }
+        return hash;
     }
 }
